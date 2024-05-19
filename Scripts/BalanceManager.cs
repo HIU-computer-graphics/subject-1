fa5554c638426961dc01f1edb7790d7231135631
garakjho <165467349+garakjho@users.noreply.github.com>
2024-05-19 19:50:19 +0900

    Add files via upload

---
+++ Scripts/BalanceManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using DG.Tweening;
+using TMPro;
+
+public class BalanceManager : MonoBehaviour {
+
+    [SerializeField]
+    private float balance = 0;
+    public TMP_Text balanceText;
+
+    private static BalanceManager _Instance;
+
+    void Start()
+    {
+        _Instance = this;
+        balanceText.text = balance.ToString("F2");
+    }
+
+    public static void ChangeBalance(float value)
+    {
+        _Instance.balance += value;
+        _Instance.balanceText.text = _Instance.balance.ToString("F2");
+    }
+
+    public void SetBalanceAt(float value)
+    {
+        DOTween.To(() => balance, x => balance = x, value, 1).SetEase(Ease.InOutSine).OnUpdate(() => { balanceText.text = balance.ToString("0"); });
+    }
+
+    public static double GetBalance()
+    {
+        return _Instance.balance;
+    }
+
+}
---
+++ Scripts/Card.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using DG.Tweening;
+public class Card : MonoBehaviour {
+
+    public CardData cardData;
+
+    private Animator animator;
+    private MeshRenderer renderCard;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        renderCard = GetComponent<MeshRenderer>();
+    }
+
+    public void MoveTo(Vector3 target)
+    {
+        DOTween.Sequence()
+                .Append(transform.DOMove(target, 1))
+                .Join((transform.DORotate(new Vector3(0, 0, 180), .5f))).OnComplete(() => {
+                    Destroy(gameObject);
+                });
+    }
+}
